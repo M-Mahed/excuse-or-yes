d19126e676cf714ce646cc7b1e8cc2156325bb1d
M-Mahed <mahedhafeez1@gmail.com>
2025-09-07 00:53:00 +0500

    Update en.tsx

--- src/i18n/resources/en.tsx
+++ src/i18n/resources/en.tsx
@@ -6,13 +6,13 @@ export const translations: Translations<"en"> = {
         "No": "No",
     },
     "FirstSticker": {
-        "Will you excuse me?": "Will you excuse me?"
+        "Will you forgive me?": "Will you excuse me?"
     },
     "SecondSticker": {
         "Really?": "Really?"
     },
     "ThirdSticker": {
-        "Will you call me?": "Will you call me?"
+        "Will you forgive me?": "Will you call me?"
     },
     "Call": {
         "Yes": "Yes",
